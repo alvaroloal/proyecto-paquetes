1bba6ab7b4d0cc49c46bb34e4c8d7e710a632638
Álvaro Lorente Almán <150147467+alvaroLorente1@users.noreply.github.com>
2024-05-12 19:38:08 +0200

    Añadiendo el repositorio del proyecto

---
+++ ProyectoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectoalvarolorentealman/ProyectoAlvaroLorenteAlmanApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoalvarolorentealman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAlvaroLorenteAlmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAlvaroLorenteAlmanApplication.class, args);
+	}
+
+}
---
+++ ProyectoAlvaroLorenteAlman/src/test/java/com/salesianostriana/dam/proyectoalvarolorentealman/ProyectoAlvaroLorenteAlmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoalvarolorentealman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAlvaroLorenteAlmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
