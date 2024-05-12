6ccc4a4c3cf47792f9c4cbc1bed4e6743ba5ec6e
Álvaro Lorente Almán <150147467+alvaroLorente1@users.noreply.github.com>
2024-05-12 19:31:48 +0200

    Delete ProyectoPaqueteriaAlvaroLorenteAlman directory

--- ProyectoPaqueteriaAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectopaqueteriaalvarolorentealman/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectopaqueteriaalvarolorentealman.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectopaqueteriaalvarolorentealman.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
-}
--- ProyectoPaqueteriaAlvaroLorenteAlman/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into cliente (id, nombre, apellidos, email, telefono, direccion, numeroTarjeta) values (1, 'Álvaro', 'Lorente Almán', 'alvaro0@aol.com', '686544477','Plaza Pedro Santos Gomez', '1234455678');
-insert into cliente (id, nombre, apellidos, email, telefono, direccion, numeroTarjeta) values (2, 'Manuel', 'Lorente Almán', 'manuel0@aol.com', '686544477','Denver, Colorado', '1234455678');
-insert into cliente (id, nombre, apellidos, email, telefono, direccion, numeroTarjeta) values (3, 'Eva', 'Almán Ruiz', 'alvaro0@aol.com', '686544477','Plaza Pedro Santos Gomez', '1234455678');
