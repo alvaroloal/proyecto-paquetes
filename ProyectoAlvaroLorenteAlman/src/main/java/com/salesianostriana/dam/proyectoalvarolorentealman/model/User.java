82d3790e9154c56dc02455b83579aecf84eac5da
Álvaro Lorente Almán <150147467+alvaroLorente1@users.noreply.github.com>
2024-05-14 19:49:17 +0200

    modificando user

--- ProyectoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectoalvarolorentealman/model/User.java
+++ ProyectoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectoalvarolorentealman/model/User.java
@@ -1,13 +1,16 @@
 package com.salesianostriana.dam.proyectoalvarolorentealman.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -15,12 +18,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name="user")
 public abstract class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private Long id;
 	
+	
+	@Column(name="userName")
 	private String userName;
+	
+	@Column(name="password")
 	private String password;
 	
 
